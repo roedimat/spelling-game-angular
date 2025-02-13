5f6b0b2f3d03bda44921b8e6e07de4499118c914
roedimat <matt.roed23@gmail.com>
2025-02-12 19:09:14 -0600

    ng add @angular/fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "spelling-game-d703c", appId: "1:174837114785:web:3361cb1294720de5a90de0", storageBucket: "spelling-game-d703c.firebasestorage.app", apiKey: "AIzaSyB-zH3VexSIMj03c4y8JK0rD9nEHCbxy5k", authDomain: "spelling-game-d703c.firebaseapp.com", messagingSenderId: "174837114785", measurementId: "G-Q3CLC94G79" })), provideAnalytics(() => getAnalytics()), ScreenTrackingService, provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
 };
